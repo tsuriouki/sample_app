60734f844624f8882b82c18ad9da68b5419f69a3
Oki Ichitsuri <oki.ichitsuri.9q@stu.hosei.ac.jp.com>
2025-04-21 06:54:19 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,17 @@ module ActiveSupport
     include ApplicationHelper
     # Add more helper methods to be used by all tests here...
   end
+
+end
+
+class ActiveSupport::TestCase
+  # 指定のワーカー数でテストを並列実行する
+  parallelize(workers: :number_of_processors)
+  # test/fixtures/*.ymlのfixtureをすべてセットアップする
+  fixtures :all
+
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
