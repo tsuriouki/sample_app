d6b0d8b974b384d2ac58da8a67ab3a65901b8e44
Oki Ichitsuri <oki.ichitsuri.9q@stu.hosei.ac.jp.com>
2025-04-30 08:15:31 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,15 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
 
   # 臨時で追加 URLから特定ユーザーをdeleteできるため、本番環境では本来絶対に実装してはいけない！！
